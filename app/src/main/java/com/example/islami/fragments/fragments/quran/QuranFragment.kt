e4c1ff8067a21cdab980ba2a412ee87acd938e57
Abdelrahman <abdelrahmankhaled.swengineering@gmail.com>
2024-01-22 20:43:23 +0200

    In this commit i have finished the bottom navigation bar. Also i have created 4 empty fragments for quran , ahadeth , sebha , radio.

---
+++ app/src/main/java/com/example/islami/fragments/fragments/quran/QuranFragment.kt
@@ -0,0 +1,26 @@
+package com.example.islami.fragments.fragments.quran
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.islami.R
+import com.example.islami.databinding.FragmentQuranBinding
+
+
+class QuranFragment : Fragment() {
+    lateinit var binding: FragmentQuranBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentQuranBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
